7cced5082da7c08ff02127644d75f22c8181f8eb
Zachary Kasprzyk <139936668+Zach-Kasp@users.noreply.github.com>
2023-07-20 11:02:01 -0400

    Add files via upload
    Realized I made a mistake on 1672 as it was not a jagged array.

--- 1672.cs
+++ 1672.cs
@@ -10,32 +10,38 @@ int iValueMin = 0;
 int IValueMax = 100;
 
 int iCustomer = rnd.Next(iCustomerMin, iCustomerMax);
-int iBank = rnd.Next(iBankMin, IBankMax);
+int iBank = default;
 
-int[,] iAccounts = new int[iCustomer, iBank];
+int[][] iAccounts = new int[iCustomer][];
 
 for (int i = 0; i < iCustomer; i++)
 {
+    iBank = rnd.Next(iBankMin, IBankMax);
+    iAccounts[i] = new int[iBank];
     Console.Write("Row {0:D}: ", i);
     for (int j = 0; j < iBank; j++)
     {
-        iAccounts[i, j] = rnd.Next(iValueMin, IValueMax);
-        Console.Write("{0:D} ", iAccounts[i, j]);
+        int iTempValue = rnd.Next(iValueMin, IValueMax);
+        iAccounts[i][j] = iTempValue;
+        Console.Write("{0:D} ", iAccounts[i][j]);
     }
 
     Console.WriteLine();
 }
 
+
 int[] iWealths = new int[iCustomer];
 
-for (int i = 0; i < iCustomer; i++)
+
+for (int i = 0; i < iAccounts.GetLength(0); i++)
 {
-    for (int j = 0; j < iBank; j++)
+    for (int j = 0; j < iAccounts[i].Length; j++)
     {
-        iWealths[i] += iAccounts[i, j];
+        iWealths[i] += iAccounts[i][j];
     }
     Console.WriteLine("{0:D} ", iWealths[i]);
 }
+
 
 int iMaxValue = 0;
 int iRichestCustomer = 0;
