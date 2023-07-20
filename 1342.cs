45ae684f9f5deabeb0b21a3f98c6eaf1a02d8caf
Zachary Kasprzyk <139936668+Zach-Kasp@users.noreply.github.com>
2023-07-20 12:28:00 -0400

    Add files via upload

---
+++ 1342.cs
@@ -0,0 +1,29 @@
+﻿Random rnd = new Random();
+
+int iNumberMin = 1;
+
+
+//Convert.ToInt32(10e6);
+int iNumberMax = 100;
+
+int iNumber = rnd.Next(iNumberMin, iNumberMax);
+
+int iCounter = 0;
+
+Console.WriteLine(iNumber);
+
+while (iNumber != 0)
+{
+    iCounter++;
+    if (iNumber % 2 == 0)
+    {
+        iNumber = iNumber / 2;
+    }
+    else
+    {
+        iNumber = iNumber - 1;
+    }
+    Console.WriteLine(iNumber);
+}
+
+Console.WriteLine("\nNumber of Steps required: {0:D}",iCounter);
